00ced17b6f0dc5c0acc37556cfe59d193edc1ca0
Saurabh <saurabhchavhan07@gmail.com>
2022-06-06 21:18:24 +0530

    Aaray of marks

---
+++ Array/Marks.java
@@ -0,0 +1,13 @@
+public class Marks{
+    public static void main(String args[]){
+        int mark[] = new int[3];
+        mark[0]=97;
+        mark[1]=95;
+        mark[2]=94;
+        // mark[3]=93;
+
+        for(int i = 0; i<=2; i++){
+            System.out.println(mark[i]);
+        }
+    }
+}
